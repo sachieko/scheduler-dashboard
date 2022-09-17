ea9d162cee031824e47ab0d67371a770d1a49d83
sachieko <arashikabane@gmail.com>
2022-09-16 18:40:45 -0600

    Fetch data from API server

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 Install dependencies with `npm install`.
 
+NOTE: Due to port conflictions, the proxy's port in package.json is 8080 rather than the default, and my local development ports in .env also changed. 
+You should know what you're doing with ports if you'd like to use this react app.
+
 ## Running Webpack Development Server
 
 ```sh
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,37 +1,70 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
-import classnames from "classnames";
-
-import Loading from "./Loading";
-import Panel from "./Panel";
+import Loading from './Loading';
+import Panel from './Panel';
 
 const data = [
   {
     id: 1,
-    label: "Total Interviews",
+    label: 'Total Interviews',
     value: 6
   },
   {
     id: 2,
-    label: "Least Popular Time Slot",
-    value: "1pm"
+    label: 'Least Popular Time Slot',
+    value: '1pm'
   },
   {
     id: 3,
-    label: "Most Popular Day",
-    value: "Wednesday"
+    label: 'Most Popular Day',
+    value: 'Wednesday'
   },
   {
     id: 4,
-    label: "Interviews Per Day",
-    value: "2.3"
+    label: 'Interviews Per Day',
+    value: '2.3'
   }
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
+  }
+
+  componentDidMount() {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ])
+      .then(([days, appointments, interviewers]) => {
+        this.setState({
+          loading: false,
+          days: days.data,
+          appointments: appointments.data,
+          interviewers: interviewers.data
+        });
+      })
+      .catch(err => console.log('Uh oh, something went horribly wrong: ', err.message));
+
+
+    const focused = JSON.parse(localStorage.getItem('focused'));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem('focused', JSON.stringify(this.state.focused));
+    }
   }
 
   selectPanel(id) {
@@ -39,12 +72,14 @@ class Dashboard extends Component {
   }
 
   render() {
-    const dashboardClasses = classnames("dashboard", { "dashboard--focused": this.state.focused });
+    const dashboardClasses = classnames('dashboard', { 'dashboard--focused': this.state.focused });
 
     if (this.state.loading) {
       return <Loading />
     }
 
+    console.log(this.state);
+    
     const panels = (this.state.focused ? data.filter(panel=> this.state.focused === panel.id) : data).map(panel => {
       return <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} onSelect={event => this.selectPanel(panel.id)} />
     });
